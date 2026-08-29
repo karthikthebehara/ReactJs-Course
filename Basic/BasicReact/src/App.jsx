a3481158c2d562ff07626213933938fdea2c681d
karthikthebehara <karthikbehara2005@gmail.com>
2026-08-29 18:40:49 +0530

    Recat Js Course Initial Commit

---
+++ Basic/BasicReact/src/App.jsx
@@ -0,0 +1,19 @@
+
+const App = () => {
+  return (
+    <>
+    
+    <div id="father">
+
+      <div id="child1">Iam Child 1</div>
+      <div id="child2">I am Child 2</div>
+    </div>
+    <div id="gradParent">
+      I am grand Parent
+    </div>
+    
+    </>
+  )
+}
+
+export default App
